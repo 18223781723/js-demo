f3a04bd82eca7245d3a041d1d8c5b2147c1673bd
18223781723 <1461301522@qq.com>
2021-05-13 19:09:19 +0800

    文件分类

---
+++ js/data.js
@@ -0,0 +1,86 @@
+const tableList = [
+	{
+		title: 'date range',
+		type: '原理',
+		description: '日期区间选择器，常见于酒店选择"入住 - 退房"日期',
+		href: 'html/principle/dateRange.html',
+		createDate: '2020-10-28'
+	},
+	{
+		title: 'image compress',
+		type: '原理',
+		description: '图片压缩基本原理',
+		href: 'html/principle/imageCompress.html',
+		createDate: '2020-10-29'
+	},
+	{
+		title: 'calendar',
+		type: '原理',
+		description: '日历实现',
+		href: 'html/principle/calendar.html',
+		createDate: '2020-10-29'
+	},
+	{
+		title: 'forEach',
+		type: '原理',
+		description: '数组forEach方法基本原理',
+		href: 'html/principle/forEach.html',
+		createDate: '2020-10-29'
+	},
+	{
+		title: 'picker',
+		type: '原理',
+		description: 'picker选择器实现',
+		href: 'html/principle/picker.html',
+		createDate: '2020-10-29'
+	},
+	{
+		title: 'debounce',
+		type: '原理',
+		description: '防抖函数基本原理',
+		href: 'html/principle/debounce.html',
+		createDate: '2020-10-30'
+	},
+	{
+		title: 'throttle',
+		type: '原理',
+		description: '节流函数基本原理',
+		href: 'html/principle/throttle.html',
+		createDate: '2020-10-30'
+	},
+	{
+		title: 'call',
+		type: '原理',
+		description: 'call基本原理',
+		href: 'html/principle/call.html',
+		createDate: '2020-10-30'
+	},
+	{
+		title: 'apply',
+		type: '原理',
+		description: 'apply基本原理',
+		href: 'html/principle/apply.html',
+		createDate: '2020-10-30'
+	},
+	{
+		title: 'slider',
+		type: '原理',
+		description: 'slider滑块实现',
+		href: 'html/principle/slider.html',
+		createDate: '2020-10-30'
+	},
+	{
+		title: 'event delegation',
+		type: '原理',
+		description: '事件委托原理',
+		href: 'html/principle/eventDelegation.html',
+		createDate: '2020-11-03'
+	},
+	{
+		title: 'onion model',
+		type: '原理',
+		description: '洋葱模型实现',
+		href: 'html/principle/onionModel.html',
+		createDate: '2021-05-12'
+	},
+];
---
+++ js/index.js
@@ -0,0 +1,23 @@
+Page({
+	data: {},
+	onLoad: function () {
+		this.renderTable();
+	},
+	renderTable: function () {
+		let html = '';
+		for (let item of tableList) {
+			html += `
+			<tr>
+				<td>${item.title}</td>
+				<td>${item.type}</td>
+				<td>${item.description}</td>
+				<td>${item.createDate}</td>
+				<td><a href="${item.href}">detail</a></td>
+			</tr>`;
+		}
+		document.getElementById('content').innerHTML = html;
+	},
+})
+function Page(options) {
+	options.onLoad();
+}
